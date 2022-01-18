5a2c7ad11543743f63c0c5c7c49739bdcb352584
Gigi <97762890+GigiMOla@users.noreply.github.com>
2022-01-19 00:15:34 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+var Player = document.getElementById('player');
+var Score = document.getElementById('score');
+var Timer = document.getElementById('timer')
+var RealTimer = 20;
+var RealScore = 0;
+var IntervalCounter = 800;
+var Sound = new Audio();
+Sound.src = '/Audio/pop.wav'
+
+var WinAudio = new Audio()
+WinAudio.src = '/Audio/Win.wav'
+
+var Lose = new Audio()
+Lose.src = '/Audio/lose.mp3'
+
+
+var Interval = setInterval(StartTimer, IntervalCounter)
+
+Player.addEventListener('click', function() {
+    Sound.play()
+    RealScore++;
+    Score.textContent = 'Score' + ' ' + RealScore;
+    var RandomPosX = Math.floor(Math.random() * 500);
+    var RandomPosY = Math.floor(Math.random() * 600);
+    var RandomScale = Math.floor(Math.random() * 100);
+
+
+    if (RandomScale < 10) {
+        RandomScale = 20;
+        Player.style.width = RandomScale + 'px';
+        Player.style.height = RandomScale + 'px';
+    }
+
+    Player.style.marginLeft = RandomPosX + 'px';
+    Player.style.marginTop = RandomPosY + 'px';
+    Player.style.width = RandomScale + 'px';
+    Player.style.height = RandomScale + 'px';
+})
+
+function StartTimer() {
+    RealTimer--;
+    Timer.textContent = 'Timer' + ' ' + RealTimer;
+
+    if (RealTimer <1) {
+        Lose.play()
+        EndGame()
+        Win()
+    }
+}
+
+function StopTimer() {
+
+}
+function EndGame() {
+    clearInterval(Interval)
+}
+function Win() {
+    if (RealScore > 20) {
+        WinAudio.play()
+        Swal.fire(
+            'Good job!',
+            'You Win, Your score is' + ' ' + RealScore,
+            'success'
+          )
+        EndGame()
+    } else {
+        Lose.play()
+        Swal.fire('Sorry, you lose game')
+    }
+} 
